b5cf92333a2b3a18c2ef9b4753c3a10ca942ade8
lcardozav <lcardozav@gmail.com>
2022-01-02 16:15:07 -0600

    Primer guardado en git del proyecto Javapptuyu

---
+++ JavAppTuyu/src/javapptuyu/JavAppTuyu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapptuyu;
+
+/**
+ *
+ * @author lcard
+ */
+public class JavAppTuyu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
